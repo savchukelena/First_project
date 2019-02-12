94a2f039c89c02813f79e4b0617b40646c5bcfea
Elena S <savchuk-elena@list.ru>
2019-02-12 10:34:14 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -5,3 +5,18 @@ Created on Tue Feb 12 10:02:36 2019
 @author: Савчук Елена
 """
 
+import pyarrow.parquet as parquet
+# Used to train the baseline model
+
+from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
+
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
+
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+
+
+
